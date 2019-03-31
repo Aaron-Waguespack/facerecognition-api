fc40d6d13d71200fbf79570623f8e5047726617b
Aaron Waguespack <Aaron_Waguespack@hotmail.com>
2019-03-31 13:56:39 -0600

    adding server connection

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(cors());
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'password',
-    database : 'Face Recognition'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
